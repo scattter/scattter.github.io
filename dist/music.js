afada489273cdbc3f67781d6e3058589a4f79899
scattter <51482472+scattter@users.noreply.github.com>
2020-01-02 20:58:42 +0800

    Site updated: 2020-01-02 20:58:41

--- dist/music.js
+++ dist/music.js
@@ -23,3 +23,16 @@ const ap = new APlayer({
       }
     ]
 });
+
+const colorThief = new ColorThief();
+const setTheme = (index) => {
+    if (!ap.list.audios[index].theme) {
+        colorThief.getColorAsync(ap.list.audios[index].cover, function (color) {
+            ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
+        });
+    }
+};
+setTheme(ap.list.index);
+ap.on('listswitch', (index) => {
+    setTheme(index);
+});
