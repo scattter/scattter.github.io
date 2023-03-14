8d3c8ef620e60d04a2a082f9bc05324b54f36e58
张柯 <413365742@qq.com>
2023-03-15 00:00:20 +0800

    commit forget file: common.ts

--- docs/types/common.ts
+++ docs/types/common.ts
@@ -1,3 +1,10 @@
+export type { DirectiveBinding } from 'vue'
+
+export interface ElType extends HTMLElement {
+  copyData: string | number
+  __handleClick__: any
+}
+
 export interface VitePressConfig {
   text: string
   items?: VitePressConfig[]
