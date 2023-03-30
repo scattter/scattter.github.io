7be824fa97ae08884c01b411b3b603576fac7fec
张柯 <413365742@qq.com>
2023-03-30 21:04:33 +0800

    update missing file to site

--- docs/.vitepress/config/jsConfig.ts
+++ docs/.vitepress/config/jsConfig.ts
@@ -1,6 +1,7 @@
 import { VitePressConfig, VitePressNav } from '@/types/common'
 
 const JS_CONFIG_PATTERNS_PATH = '/js/designPatterns'
+const JS_CONFIG_VUE_PATH = '/js/vue'
 
 
 export function sidebarJsConfig(): VitePressConfig[] {
@@ -16,6 +17,14 @@ export function sidebarJsConfig(): VitePressConfig[] {
         { text: '原型链模式', link: `${JS_CONFIG_PATTERNS_PATH}/prototype/index` },
         { text: '模块模式', link: `${JS_CONFIG_PATTERNS_PATH}/module/index` },
         { text: '混入模式', link: `${JS_CONFIG_PATTERNS_PATH}/mixin/index` },
+        { text: '单例模式', link: `${JS_CONFIG_PATTERNS_PATH}/singleton/index` },
+      ]
+    },
+    {
+      text: 'vue相关',
+      collapsible: true,
+      items: [
+        { text: 'tooltip指令', link: `${JS_CONFIG_VUE_PATH}/directives/tooltip/index` },
       ]
     },
   ]
@@ -27,6 +36,11 @@ export const navJsConfig: VitePressNav = {
     {
       text: '设计模式',
       link: '/js/designPatterns/index',
+      activeMatch: '/js/'
+    },
+    {
+      text: 'vue相关',
+      link: `${JS_CONFIG_VUE_PATH}/directives/tooltip/index`,
       activeMatch: '/js/'
     },
   ]
